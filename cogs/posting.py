fcef3fd88dde2d40f146b181e459cbedc8bb01fb
Timm <96317759+Timm04@users.noreply.github.com>
2022-08-26 02:05:29 +0200

    Fixed buggs

--- cogs/posting.py
+++ cogs/posting.py
@@ -6,15 +6,17 @@ import datetime
 from datetime import timedelta
 import pytz
 import asyncio
+import json
 
 #############################################################
 
-#files
-guild_id = 947813835715256390 #TMW GUILD ID
-look_back_days = 7
-wait_time = 21600
+with open("cogs/channel_ids.json") as json_file:
+    data_dict = json.load(json_file)
+    look_back_days = data_dict["look_back_days"] # 7 days
+    wait_time = data_dict["wait_time"] # 6hrs
 
 #############################################################
+
 
 class Posting(commands.Cog):
 
@@ -23,10 +25,10 @@ class Posting(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.myguild = self.bot.get_guild(guild_id)
         self.log_channel = discord.utils.get(self.myguild.channels, name="bookmark-list")
+        self.log_channel_id = self.log_channel.id
 
-    async def delete(self, delete):
+    async def delete(self, ctx):
         con = sqlite3.connect('bookmarked-messages.db')
         cur = con.cursor()
         cur.execute(f"SELECT * FROM bookmarked_messages")
@@ -36,42 +38,45 @@ class Posting(commands.Cog):
 
     async def recursion(self, ctx):
         await asyncio.sleep(wait_time)
-        await self.delete(ctx)
-        await self.create_messge(ctx)
 
-    async def create_messge(self, ctx):
+        await self.delete(ctx)
+        await self.create_message(ctx)
+
+    async def create_message(self, ctx):
         channel = self.log_channel
-        utc=pytz.UTC
+        utc = pytz.UTC
         startw0 = datetime.datetime.today()
         endw1 = startw0 - timedelta(look_back_days)
 
         con = sqlite3.connect('bookmarked-messages.db')
         cur = con.cursor()
-        cur.execute(f"SELECT * FROM bookmarked_messages ORDER BY bookmarks DESC")
+        cur.execute(
+            f"SELECT * FROM bookmarked_messages ORDER BY bookmarks DESC")
         info = cur.fetchall()
 
         count = 0
         for (user_id, message_id, reaction_amount, content, link, create_date, attachments, keywords) in info:
             message = await channel.fetch_message(message_id)
             if message.created_at < utc.localize(startw0) and message.created_at > utc.localize(endw1):
-                red_embed = discord.Embed(title=f'__**{count}#**__     {reaction_amount} 🔖       {keywords}',description=f'{content} \n [Link]({link})', color=discord.Color.from_rgb(255, 0, 0))
+                red_embed = discord.Embed(title=f'__**{count}#**__     {reaction_amount} 🔖       {keywords}',
+                                          description=f'{content} \n [Link]({link})', color=discord.Color.from_rgb(255, 0, 0))
                 user = await self.bot.fetch_user(int(user_id))
                 pfp = user.avatar.url
-                red_embed.set_footer(icon_url=(pfp), text=f'From {user.name}  |  Posted at {create_date}')
+                red_embed.set_footer(
+                    icon_url=(pfp), text=f'From {user.name}  |  Posted at {create_date}')
                 if attachments.endswith(".png") or attachments.endswith(".jpg") or attachments.endswith(".jpeg"):
-                    red_embed.set_thumbnail(url=attachments)    
+                    red_embed.set_thumbnail(url=attachments)
                 await channel.send(embed=red_embed)
             else:
-                my_embed = discord.Embed(title=f'__**{count}#**__     {reaction_amount} 🔖       {keywords}',description=f'{content} \n [Link]({link})')
+                my_embed = discord.Embed(
+                    title=f'__**{count}#**__     {reaction_amount} 🔖       {keywords}', description=f'{content} \n [Link]({link})')
                 user = await self.bot.fetch_user(int(user_id))
                 pfp = user.avatar.url
-                my_embed.set_footer(icon_url=(pfp), text=f'From {user.name}  |  Posted at {create_date}')
+                my_embed.set_footer(
+                    icon_url=(pfp), text=f'From {user.name}  |  Posted at {create_date}')
                 if attachments.endswith(".png") or attachments.endswith(".jpg") or attachments.endswith(".jpeg"):
-                    my_embed.set_thumbnail(url=attachments)    
+                    my_embed.set_thumbnail(url=attachments)
                 await channel.send(embed=my_embed)
-        
-        con.commit()
-        con.close()
 
         await self.changelog(ctx)
 
@@ -80,25 +85,28 @@ class Posting(commands.Cog):
         date = date.strftime("%Y/%m/%d %H:%M:%S")
         con = sqlite3.connect('bookmarked-messages.db')
         cur = con.cursor()
-        cur.execute(f"SELECT bookmarks, link FROM bookmarked_messages ORDER BY bookmarks ASC LIMIT 1")
+        cur.execute(
+            f"SELECT bookmarks, link FROM bookmarked_messages ORDER BY bookmarks ASC LIMIT 1")
         first_list_link = cur.fetchone()[1]
-        info_embed = discord.Embed(title=f'Jump to the highest bookmarked message', color=discord.Color.from_rgb(255, 255, 255), url=first_list_link)
+        info_embed = discord.Embed(title=f'Jump to the highest bookmarked message',
+                                   color=discord.Color.from_rgb(255, 255, 255), url=first_list_link)
         info_embed.add_field(name=f'Changelog', value=f'- Categorization of each list item (700 most occuring words in resource-sharing, DM the bot with "t.words_db" for the DB) \n - Red coloring on embed indicates that the message is not older than a week \n - Attachment filename and image is now displayed \n - Post date of message was added')
         user = ctx.message.author
-        pfp = user.avatar_url
-        info_embed.set_footer(icon_url=(pfp), text=f'From {ctx.message.author}  |  List from {date}')
-        await self.log_channel.send(embed=info_embed)
+        pfp = user.avatar.url
+        info_embed.set_footer(
+            icon_url=(pfp), text=f'From {ctx.message.author}  |  List from {date}')
+
+        return info_embed
 
     async def changelog(self, ctx):
-        await self.create_embed(ctx)
-        await self.log_channel.send()
+        info_embed = await self.create_embed(ctx)
+        await self.log_channel.send(embed=info_embed)
         await self.recursion(ctx)
 
     @commands.command()
-    @commands.has_any_role("Moderator", "Administrator", "第四話　雨、逃げ出した後")
     async def start(self, ctx):
-        print("starting")
-        await self.created_messge(ctx)
+        await self.create_message(ctx)
+
 
 async def setup(bot):
     await bot.add_cog(Posting(bot))
