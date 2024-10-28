681227826a91bf3c58177ad78eb7114a2c115b17
Timm04 <majboroda46v@hotmail.com>
2024-10-28 14:43:09 +0100

    enhancement: bookmark files

--- cogs/bookmarker.py
+++ cogs/bookmarker.py
@@ -27,37 +27,40 @@ class Extras(commands.Cog):
         message_link = message_link.split('/')
         channel = await self.bot.fetch_channel(message_link[5])
         reaction_message = await channel.fetch_message(message_link[6])
+        bookmark = False
         for reaction in reaction_message.reactions:
             async for user in reaction.users():
                 if interaction.user == user and reaction.emoji == '🔖':
-                    guild = self.bot.get_guild(guild_id)
-                    author = reaction_message.author
-                    reacted_at = datetime.now()
-                    embed = discord.Embed(title=f"New 🔖 saved from {guild}'s {channel.name} channel", description=f'You added this bookmark on {reacted_at.strftime("%a %d %H:%M:%S")}\n', color=discord.Color.blurple())
-                    embed.add_field(name='Link', value=f'{reaction_message.jump_url}', inline=False)
-                    if len(reaction_message.content) > 900:
-                        embed.add_field(name="Content",value=f'{reaction_message.content[:900]}', inline=False)                
-                    if len(reaction_message.content) < 900:
-                        embed.add_field(name="Content",value=f'{reaction_message.content}', inline=False)
-                    try:
-                        pfp = author.avatar.url
-                    except:
-                        embed.set_footer(text=f'From {author}  |  Posted on {reaction_message.created_at.strftime("%a %d %H:%M:%S")}')
-                    else:
-                        embed.set_footer(icon_url=(pfp), text=f'From {author}  |  Posted on {reaction_message.created_at.strftime("%a %d %H:%M:%S")}')
-                    if reaction_message.attachments:
-                        if reaction_message.attachments[0].filename.endswith(".png") or reaction_message.attachments[0].filename.endswith(".jpg") or reaction_message.attachments[0].filename.endswith(".jpeg"):
-                            url = reaction_message.attachments[0].proxy_url
-                            embed.set_image(url=url)
-                        if reaction_message.attachments[0].filename.endswith('.zip') or reaction_message.attachments[0].filename.endswith('.txt') or reaction_message.attachments[0].filename.endswith('.apkg') or reaction_message.attachments[0].filename.endswith('.mp4') or reaction_message.attachments[0].filename.endswith('.mp3'):
-                            attachments = '\n'.join([attachment.filename for attachment in reaction_message.attachments])
-                            embed.add_field(name='File', value=f"{attachments}")
-                    try:
-                        await interaction.user.send(embed=embed)
-                    except discord.errors.Forbidden:
-                        return await interaction.response.send_message(ephemeral=True, content=f'<@{interaction.user.id}> please change your privacy settings to ``Allow direct messages from server members``. Jump back to the bookmarked message {reaction.message.jump_url}.', suppress_embeds=True)
-                    else:
-                        return await interaction.response.send_message(ephemeral=True, content=f'A DM has been sent to you. Jump back to the bookmarked message {reaction.message.jump_url}.', suppress_embeds=True)
+                    bookmark = True
+        if bookmark:
+            guild = self.bot.get_guild(guild_id)
+            author = reaction_message.author
+            reacted_at = datetime.now()
+            embed = discord.Embed(title=f"New 🔖 saved from {guild}'s {channel.name} channel", description=f'You added this bookmark on {reacted_at.strftime("%a %d %H:%M:%S")}\n', color=discord.Color.blurple())
+            embed.add_field(name='Link', value=f'{reaction_message.jump_url}', inline=False)
+            if len(reaction_message.content) > 900:
+                embed.add_field(name="Content",value=f'{reaction_message.content[:900]}', inline=False)                
+            if len(reaction_message.content) < 900:
+                embed.add_field(name="Content",value=f'{reaction_message.content}', inline=False)
+            try:
+                pfp = author.avatar.url
+            except:
+                embed.set_footer(text=f'From {author}  |  Posted on {reaction_message.created_at.strftime("%a %d %H:%M:%S")}')
+            else:
+                embed.set_footer(icon_url=(pfp), text=f'From {author}  |  Posted on {reaction_message.created_at.strftime("%a %d %H:%M:%S")}')
+            if reaction_message.attachments:
+                if reaction_message.attachments[0].filename.endswith(".png") or reaction_message.attachments[0].filename.endswith(".jpg") or reaction_message.attachments[0].filename.endswith(".jpeg"):
+                    url = reaction_message.attachments[0].proxy_url
+                    embed.set_image(url=url)
+                upload_files: list[discord.File] = [await attachment.to_file(filename=attachment.filename) for attachment in reaction_message.attachments if not attachment.filename.endswith('.jpeg') or attachment.filename.endswith(".jpg") or attachment.filename.endswith(".png")]
+            try:
+                await interaction.user.send(embed=embed, files=upload_files)
+            except discord.errors.Forbidden:
+                return await interaction.response.send_message(ephemeral=True, content=f'<@{interaction.user.id}> please change your privacy settings to ``Allow direct messages from server members``. Jump back to the bookmarked message {reaction.message.jump_url}.', suppress_embeds=True)
+            except Exception: #file too big
+                return await interaction.user.send(embed=embed)
+            else:
+                return await interaction.response.send_message(ephemeral=True, content=f'A DM has been sent to you. Jump back to the bookmarked message {reaction.message.jump_url}.', suppress_embeds=True)
         await interaction.response.send_message(ephemeral=True, content=f'You need to react with a 🔖 to the message you want to bookmark. Jump back to the bookmarked message {reaction.message.jump_url}.', suppress_embeds=True)
     
     # @app_commands.command(name='rm_bookmark', description='Removes one bookmark in DM.')  
